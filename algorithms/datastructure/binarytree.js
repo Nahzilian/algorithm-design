1b32ba11e14d80d3f3399d6fefe5cf7c04dd2214
Nahzilian <nahz3112@gmail.com>
2021-03-20 17:32:08 -0400

    Added function

--- algorithms/datastructure/binarytree.js
+++ algorithms/datastructure/binarytree.js
@@ -28,22 +28,80 @@ class BinaryTree {
         else this.insertNode(this.root, newNode); 
     } 
 
-    getRootNode () {
-        console.log(this.root);
+    remove (data) {
+        this.root = this.removeNode(this.root, data);
     }
+
+    removeNode(node, key) {
+        if (node === null) return;
+        else if (key < node.data) {
+            node.left = this.removeNode(node.left, key);
+            return node;
+        }else if(key > node.data) { 
+            node.right = this.removeNode(node.right, key); 
+            return node; 
+        } else { 
+            if(node.left === null && node.right === null) 
+            { 
+                node = null; 
+                return node; 
+            } 
+            if(node.left === null) 
+            { 
+                node = node.right; 
+                return node; 
+            } 
+              
+            else if(node.right === null) 
+            { 
+                node = node.left; 
+                return node; 
+            } 
+            var aux = this.findMinNode(node.right); 
+            node.data = aux.data; 
+            node.right = this.removeNode(node.right, aux.data); 
+            return node; 
+        } 
+    }
+
+    inorder(node) 
+    { 
+        if(node !== null) 
+        { 
+            this.inorder(node.left); 
+            console.log(node.data); 
+            this.inorder(node.right); 
+        } 
+    } 
+
+    getRootNode () {
+        return this.root
+    }
+
+    find (value) {
+        this.findNode(value, this.root);
+    }
+
+    findNode (value, node) {
+        if (node === null) return false;
+        if (value === node.data) return true;
+        if (value < node.data) return this.findNode(value,node.left);
+        else if (value > node.data) return this.findNode(value,node.right);
+    }
+
 }
 
 var bt = new BinaryTree();
-bt.insert(2);
-bt.insert(12);
-bt.insert(312);
-bt.insert(1);
-bt.insert(10);
-bt.insert(80);
-bt.insert(69);
-bt.insert(32);
-bt.insert(77);
-bt.insert(88);
-
-bt.getRootNode();
+bt.insert(15); 
+bt.insert(25); 
+bt.insert(10); 
+bt.insert(7); 
+bt.insert(22); 
+bt.insert(17); 
+bt.insert(13); 
+bt.insert(5); 
+bt.insert(9); 
+bt.insert(27); 
+console.log(bt.find(5));
+console.log(bt.find(1000));
 
